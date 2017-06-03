e3787f20adb1be8de53720dee1763cad59ff2b01
Patrik Uytterhoeven <patrik@open-future.be>
2017-06-03 15:33:35 +0700

    extended for devices

---
+++ discovery-device.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import urllib
+import sys
+import json
+
+file=open('/etc/zabbix/papercut.conf','r')
+for line in file.readlines():
+    (key, sep, value) = line.partition('=')
+    if key == 'papercut_ip':
+        serverip = value[1:-2]
+    if key == 'papercut_auth':
+        serverauth = value[1:-2]
+
+
+r = { "data": [] }
+domains = []
+
+url='http://{0}/api/health/devices?{1}'.format(serverip,serverauth)
+
+response = urllib.urlopen(url)
+json_input = json.loads(response.read())
+
+def main():
+    try:
+        for i in json_input['devices']:
+            r["data"].append( {"{#DEVICENAME}": i['name']} )
+
+        print json.dumps(r, indent=2, sort_keys=True, encoding="utf-8")
+
+    except (ValueError, KeyError, TypeError):
+        print "JSON format error"
+
+if __name__ == "__main__":
+    main()
+
---
+++ discovery-devicestatus.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import urllib
+import sys
+import json
+
+file=open('/etc/zabbix/papercut.conf','r')
+for line in file.readlines():
+    (key, sep, value) = line.partition('=')
+    if key == 'papercut_ip':
+        serverip = value[1:-2]
+    if key == 'papercut_auth':
+        serverauth = value[1:-2]
+
+
+r = { "data": [] }
+domains = []
+
+url='http://{0}/api/health/devices?{1}'.format(serverip,serverauth)
+
+response = urllib.urlopen(url)
+json_input = json.loads(response.read())
+
+def main():
+  if len(sys.argv)<2:
+    print "Run as:\n{0} [devicename]".format(sys.argv[0])
+
+  else:
+    devicename = sys.argv[1]
+
+    try:
+        var = [ i["status"] for i in json_input['devices'] if i["name"] == devicename ]
+        if var == [u'OK']:
+          print 1
+        else:
+          print 0
+
+    except (ValueError, KeyError, TypeError):
+        print "JSON format error"
+        print json.dumps(r, indent=2, sort_keys=True, encoding="utf-8")
+
+if __name__ == "__main__":
+    main()
