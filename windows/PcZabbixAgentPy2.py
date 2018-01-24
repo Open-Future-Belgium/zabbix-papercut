b6177b8ab595d2375eb8df547fd13236e69ca7f5
nik2017 <nikhil.eltrx@gmail.com>
2018-01-24 23:38:58 +1100

    Updated windows *.py to make compatible any zabbix agent

--- windows/PcZabbixAgentPy2.py
+++ windows/PcZabbixAgentPy2.py
@@ -18,6 +18,9 @@ import glob
 import tarfile
 import sys
 import re
+import fnmatch
+
+
 os.chdir('c:\\zabbix')
 #create binz file all file in operations are placed here.
 if not os.path.exists('binz'):
@@ -28,19 +31,26 @@ def PcZipCopy():
     zip_ref = zipfile.ZipFile('zabbix-papercut-master.zip', 'r')
     zip_ref.extractall('tmpz')
     zip_ref.close()            
-    for file in glob.glob(r'tmpz\zabbix-papercut-master\linux\*.py'):
+    for file in glob.glob(r'tmpz\zabbix-papercut-master\linux\py2\*.py'):
         shutil.copy(file,'binz')
     shutil.copy('tmpz\zabbix-papercut-master\papercut.conf','binz')    
 
 # Untar zabbix conf file for windows and agentd.exe and place in binz file 
-def Ztarcopy(): 
-    tar = tarfile.open("zabbix-3.4.3.tar.gz", 'r:gz')
+def Ztarcopy():
+    #Find the Latest zabbix agent .tar.gz file, Find the File and define zFolder name
+    for file in os.listdir('.'):
+        if fnmatch.fnmatch(file, 'zabbix-?.?.*.tar.gz'):
+            zabbixGz = file
+    zFolder = zabbixGz[:-len('.tar.gz')]
+    tar = tarfile.open(zabbixGz, 'r:gz')
 #    print (tar.getmembers())
-    tar.extract('zabbix-3.4.3/conf/zabbix_agentd.win.conf', 'tmpz')
-    tar.extract('zabbix-3.4.3/bin/win64/zabbix_agentd.exe', 'tmpz')
+    winConfLoc = zFolder +'/conf/zabbix_agentd.win.conf'
+    agentExeLoc = zFolder +'/bin/win64/zabbix_agentd.exe'
+    tar.extract( winConfLoc, 'tmpz')
+    tar.extract( agentExeLoc, 'tmpz')
     tar.close()
-    shutil.copy('tmpz\\zabbix-3.4.3\\conf\\zabbix_agentd.win.conf','binz\\zabbix_agentd.conf')
-    shutil.copy('tmpz\\zabbix-3.4.3\\bin\\win64\\zabbix_agentd.exe','binz\\zabbix_agentd.exe')
+    shutil.copy('tmpz\\'+ zFolder +'\\conf\\zabbix_agentd.win.conf','binz\\zabbix_agentd.conf')
+    shutil.copy('tmpz\\'+ zFolder +'\\bin\\win64\\zabbix_agentd.exe','binz\\zabbix_agentd.exe')
     
 # Edit all file to make them compatible for windows
 def Zedit():    
